c6e4e679a67ae65668b722a1443d08ebddd55607
Sebastian Moreno E <92068938+SMoreno73@users.noreply.github.com>
2025-05-10 20:23:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import torch
 import requests
+import os
 from io import BytesIO
-import numpy as np
 from PIL import Image
 
-# Función para descargar el archivo desde Google Drive
-def download_file_from_google_drive(file_id):
+# Función para descargar el archivo desde Google Drive y guardarlo localmente
+def download_file_from_google_drive(file_id, destination):
     url = f"https://drive.google.com/uc?id={file_id}"
     response = requests.get(url)
     if response.status_code == 200:
-        return BytesIO(response.content)
+        with open(destination, "wb") as f:
+            f.write(response.content)
+        return destination
     else:
         st.error("Error al descargar el archivo desde Google Drive")
         return None
@@ -18,14 +20,20 @@ def download_file_from_google_drive(file_id):
 # ID del archivo de Google Drive
 file_id = '1GcaNza4l5ozH3Z8t5fMGAmpZMCw8yACp'  # Reemplazar con el ID real del archivo en Google Drive
 
-# Descargar el archivo .pth desde Google Drive
-st.write("Cargando el modelo desde Google Drive...")
-model_file = download_file_from_google_drive(file_id)
+# Ruta local temporal para guardar el modelo
+model_path = 'modelo_temporal.pth'
+
+# Descargar el archivo .pth desde Google Drive si no está ya descargado
+if not os.path.exists(model_path):
+    st.write("Cargando el modelo desde Google Drive...")
+    model_file = download_file_from_google_drive(file_id, model_path)
+else:
+    model_file = model_path
 
 if model_file:
     try:
-        # Intentar cargar el modelo con weights_only=False
-        model = torch.load(model_file, weights_only=False)
+        # Intentar cargar el modelo desde el archivo temporal
+        model = torch.load(model_file)
         model.eval()
         st.write("Modelo cargado exitosamente desde Google Drive.")
     except Exception as e:
